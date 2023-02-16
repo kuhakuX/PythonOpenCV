9406e12e2deca093c006c163b95fb91abe900eae
kuhakuX <112263004+kuhakuX@users.noreply.github.com>
2023-02-16 19:52:33 +0900

    Update BASIC2.py

--- BASIC2.py
+++ BASIC2.py
@@ -1,7 +1,7 @@
 #แสดงผลภาพ
 import cv2 # cv2 มาจาก package opencv-python ลงด้วย,install with command # pip install opencv-python
 img = cv2.imread("Pic & Videos/105194786_p0.png") # img คือตัวแปรตั้งอะไรก็ได้ cv2.imread คำสั่งอ่านรูป จาก ("Pic & Videos/105194786_p0.png")
-
+# you can change ("Pic & Videos/105194786_p0.png") to ("YOURFOLDER/YOURPICTURE.png")
 #แสดงภาพ
 cv2.imshow("P i c T u r e",img) # ดึงรูปมาจาก "ตัวแปร" img หรือก็คือ "Pic & Videos/105194786_p0.png"
 cv2.waitKey(delay=0) # 5000 = 5 sec.

b105e759b485801e20341188445cfa2c923bcab6
kuhakuX <112263004+kuhakuX@users.noreply.github.com>
2023-02-16 20:42:39 +0900

    Create BASIC7.py

---
+++ BASIC7.py
@@ -0,0 +1,13 @@
+# Open Video with OpenCV                Home _ Twitter_3.mp4
+import cv2
+
+cap = cv2.VideoCapture("Pic & Videos/Home _ Twitter_3.mp4")
+
+while (cap.isOpened()):
+    check , frame = cap.read() # รับภาพจากกล้อง frame ต่อ frame
+    cv2.imshow("Output",frame)
+    if cv2.waitKey(100) & 0xFF == ord("e"): # cv2.waitKey(100) ปรับความเร็ว Videos ได้
+        break
+
+cap.release()
+cv2.destroyAllWindows()

d18724785c412b56632831f4bb835d9850bfe7d1
kuhakuX <112263004+kuhakuX@users.noreply.github.com>
2023-02-16 19:59:34 +0900

    Update BASIC3.py

--- BASIC3.py
+++ BASIC3.py
@@ -1,1 +1,10 @@
-#การปรับเปลี่ยนขนาดภาพ / Resize Picture
+#แสดงผลภาพ
+
+import cv2 # cv2 มาจาก package opencv-python ลงด้วย,install with command # pip install opencv-python
+img = cv2.imread("Pic & Videos/105194786_p0.png") # img คือตัวแปรตั้งอะไรก็ได้ cv2.imread คำสั่งอ่านรูป จาก ("Pic & Videos/105194786_p0.png")
+img1080 = cv2.resize(img,(1920,1080))
+
+#แสดงภาพ
+cv2.imshow("P i c T u r e",img1080) # ดึงรูปมาจาก "ตัวแปร" img หรือก็คือ "Pic & Videos/105194786_p0.png"
+cv2.waitKey(delay=0) # 5000 = 5 sec.
+cv2.destroyAllWindows() # ปิดหน้าต่าง , ปิดรูป

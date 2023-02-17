010736e63ec118b8b468dc91ce01e1a9e0977fa1
kuhakuX <112263004+kuhakuX@users.noreply.github.com>
2023-02-17 11:31:14 +0900

    Update BASIC7.py

--- BASIC7.py
+++ BASIC7.py
@@ -5,11 +5,13 @@ cap = cv2.VideoCapture("Pic & Videos/Home _ Twitter_3.mp4")
 
 while (cap.isOpened()):
     check , frame = cap.read() # รับภาพจากกล้อง frame ต่อ frame
-    cv2.imshow("Output",frame)
-    if cv2.waitKey(100) & 0xFF == ord("e"): # cv2.waitKey(100) ปรับความเร็ว Videos ได้
+
+    if check == True :
+        cv2.imshow("Miku Miku Dance",frame)
+        if cv2.waitKey(100) & 0xFF == ord("e"): # cv2.waitKey(100) ปรับความเร็ว Videos ได้
+         break
+    else :
         break
 
 cap.release()
 cv2.destroyAllWindows()
-
-# https://www.youtube.com/watch?v=Pk7RoHIrAD4
